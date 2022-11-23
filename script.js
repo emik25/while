9584b691231f8dedf00c52683ba153f5a50a73fe
Emil <115448156+emik25@users.noreply.github.com>
2022-11-23 09:10:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let arr = ['shaxzod', 'valentina', 'aziz', 'odilbek', 'emil', 'saidbek']
+let i = 0
+let name = arr[i]
+
+
+while(name !== 'abbos') {
+    i++
+    console.log("Нет такого")
+    break
+}
